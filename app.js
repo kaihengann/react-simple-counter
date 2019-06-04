4f6e2c6ed26a9cc5b6389c4c30f5fc31340d2d2c
kaihengann <annkaiheng@gmail.com>
2019-06-04 13:36:53 +0800

    Duplicate components

--- app.js
+++ app.js
@@ -16,19 +16,16 @@
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { count: 0 };
-    this.incrementCount = this.incrementCount.bind(this);
+    this.state = {
+      count: 0
+    };
   }
-  
-  incrementCount() {
-    this.setState(state => ({ count: state.count + 1 })); // implicit return
-  };
 
-  // decrementCount() {
-  //   this.setState(state => {
-  //     return {count: state.count - 1};
-  //   });
-  // };
+  incrementCount = () => {
+    this.setState(state => {
+      return { count: state.count + 1 };
+    });
+  };
 
   decrementCount = () => {
     this.setState(state => {
@@ -36,17 +33,49 @@ class Counter extends React.Component {
     });
   };
 
+  bgColor = () => {
+    return this.state.count >= 0 ? "display positive" : "display negative";
+  };
+
   render() {
     return (
-      <div id="app">
-        <button>{this.state.count}</button>
+      <div id="counter">
+        <div className={this.bgColor()}> {this.state.count}</div>
         <button onClick={this.incrementCount}> + </button>
         <button onClick={this.decrementCount}> - </button>
+        <Display 
+          bgColor={this.bgColor()}
+          count={this.state.count}
+        />
+        <Button 
+          onClick={this.incrementCount}
+          text='+'
+        />
+        <Button
+          onClick={this.incrementCount}
+          text='-'
+        />
       </div>
     );
-  }
+  };
+}
+
+const Display = ({bgColor, count}) => {
+  return (
+  <div className={bgColor}>{count}</div>
+  )
+};
+const Button = ({className, onClick, text}) => {
+  return (
+    <button className={className} onClick={onClick}> {text} </button>
+  )
 };
 
+{/* <button onClick={incrementCount}> + </button>
+<button onClick={decrementCount}> - </button> */}
+
+
 const element = <Counter />;
+
 const container = document.getElementById("app");
 ReactDOM.render(element, container);
